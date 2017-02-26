83a90b1f72a125282543c25deec4811970b9fae0
Matthew Weingarten <matt@weingarten.org>
2017-02-26 14:31:28 -0800

    :floppy_disk: Improves rake command

--- lib/amazonecho/railtie.rb
+++ lib/amazonecho/railtie.rb
@@ -5,31 +5,59 @@ class AmazonEcho
     rake_tasks do
       directory "app"
       directory "app/controllers"
+      directory "config"
+
+
       task :amazonecho do
+        Rake::Task["intent_controller.rb"].invoke
+        Rake::Task["routes.rb"].invoke
+        puts "\n\nReady to build alexa skills!\n\n"
+      end
+
+
+
+
+      file "intent_controller.rb" => "app/controllers" do
+
         string =
 'class IntentController < ApplicationController
-     skip_before_action  :verify_authenticity_token
-     #Don`t forget to add all the Intent methods as helpers and to update your routes!
-     #helper_method: example
-     def intents
-       respond_to do |format|
-         format.json {
-           render json: AlexaResponder.responds_to_alexa_requests(params)
-         }
-       end
-     end
+ skip_before_action  :verify_authenticity_token
+ #Don`t forget to add all the Intent methods as helpers and to update your routes!
+ #helper_method: example
+ def intents
+   respond_to do |format|
+     format.json {
+       render json: AlexaResponder.responds_to_alexa_requests(params)
+     }
+   end
+ end
 end
 
 # add your intent methods here like this:
 # def example(alexa, slot1, slot2) etc...
-  # code here
+# code here
 # end'
 
-         file "build" => "app/controllers" do
-           sh " echo '#{string}' > 'app/controllers/intent_controller.rb'"
-        end
+
+        sh " echo '#{string}' > 'app/controllers/intent_controller.rb'"
       end
-        puts "Created file 'intent_controller'"
+
+
+
+
+
+      file "routes.rb" => "app/controllers" do
+
+        string2 = """
+Rails.application.routes.draw do
+
+  root to: 'intent#intents'
+  post '/intents', to: 'intent#intents'
+
+end"""
+
+        sh %Q! echo "#{string2}" > 'config/routes.rb'!
+      end
     end
   end
 end
